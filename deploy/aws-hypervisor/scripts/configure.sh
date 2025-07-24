5443bce496dc057973cb9c83c4497da9bd5cf610
Pablo Fontanilla <pfontani@redhat.com>
2025-07-24 10:58:28 +0200

    Merge configure.sh

--- deploy/aws-hypervisor/scripts/configure.sh
+++ deploy/aws-hypervisor/scripts/configure.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
-
-# Source the environment file to get RHSM credentials and other config
 source ~/profile.env
 
 sudo hostnamectl set-hostname aws-${STACK_NAME}
+
+function get_ocp_version() {
+    local latest_ga_ocp_version
+    latest_ga_ocp_version="$(curl -sL https://sippy.dptools.openshift.org/api/releases | jq -re '.ga_dates | to_entries | max_by(.value) | .key')"
+    if [ $? -eq 0 ]
+    then
+        echo "${latest_ga_ocp_version:-4.19}"
+    else
+        echo "4.19"
+    fi
+}
 
 user=${1:-pitadmin}
 if id "$user" >/dev/null 2>&1; then
@@ -37,4 +46,4 @@ sudo subscription-manager attach --pool=8a85f99c7d76f2fd017d96c411c70667
 sudo subscription-manager repos \
 --enable "rhel-9-for-$(uname -m)-appstream-rpms" \
 --enable "rhel-9-for-$(uname -m)-baseos-rpms" \
---enable "rhocp-4.14-for-rhel-9-$(uname -m)-rpms"
+--enable "rhocp-$(get_ocp_version)-for-rhel-9-$(uname -m)-rpms"
